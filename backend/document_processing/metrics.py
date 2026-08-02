09fd5bd8e2be3ceafc0c7d651866df14edd354bb
Manabendu-ai <rikugithub@gmail.com>
2026-08-03 01:11:15 +0530

    get metrics of a doc

--- backend/document_processing/metrics.py
+++ backend/document_processing/metrics.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import fitz
 from .models import DocumentType
 
 @dataclass
@@ -12,8 +13,29 @@ class DocumentMetrics:
 
 
 
-# class Metrcis:
+class Metrcis:
 
-#     def __init__(self, path : str):
+    def get_metrcis(self, file_path : str):
+        doc = fitz.open(file_path)
+
+        total_words = 0
+        total_characters = 0
+        for page in doc:
+            text = page.get_text().strip()
+            total_words += len(text.split())
+            total_characters += len(text)
+
+        total_pages = len(doc)
+
+        return {
+            "total_pages" : total_pages,
+            "total_words" : total_words,
+            "total_characters" : total_characters,
+            "avg_words_per_page": total_words/total_pages,
+            "avg_characters_per_page": total_characters/total_pages
+
+        }
+
+
 
 
