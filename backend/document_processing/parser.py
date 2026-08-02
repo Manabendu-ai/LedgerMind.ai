a983786cd508f80a9a81ef40aedd646652989f54
Manabendu-ai <rikugithub@gmail.com>
2026-08-03 01:54:33 +0530

    PDFParser to parse text out from pf

--- backend/document_processing/parser.py
+++ backend/document_processing/parser.py
@@ -8,3 +8,20 @@ class ParsedDocument:
     content: str
 
 
+class PDFParser:
+
+    def parse(self, file_path: str)->ParsedDocument:
+        doc = fitz.open(file_path)
+
+        pages = []
+        for page in doc:
+            text = page.get_text()
+            if text.strip():
+                pages.add(text)
+
+        file_content = "\n\n".join(pages)
+        return ParsedDocument(
+            file_name=file_path,
+            total_pages=len(doc),
+            content=file_content
+        )
