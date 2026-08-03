f573a0edfcd42f946c349e530100312cd89a38e3
Manabendu-ai <rikugithub@gmail.com>
2026-08-03 19:52:03 +0530

    Testing JSON Extractor Successfull

--- backend/test.py
+++ backend/test.py
@@ -1,13 +1,19 @@
-from document_processing.router import DocumentRouter
-from document_processing.processor import DocumentProcessor
+# from document_processing.router import DocumentRouter
+# from document_processing.processor import DocumentProcessor
 
-# router = DocumentRouter()
+# # router = DocumentRouter()
 
-# result = router.classify_document("docs/LeetCode Java Practice Solved Questions.pdf")
+# # result = router.classify_document("docs/LeetCode Java Practice Solved Questions.pdf")
 
-# print(result)
+# # print(result)
 
-document_processor = DocumentProcessor()
-result = document_processor.extract_text("docs/1000073988.pdf")
-print(result.total_pages)
-print(result.content)
+# document_processor = DocumentProcessor()
+# result = document_processor.extract_text("docs/1000073988.pdf")
+# print(result.total_pages)
+# print(result.content)
+
+from docling_processing.json_extractor import JsonExtractor
+
+extractor = JsonExtractor()
+response = extractor.extract("docs/1000073988.pdf")
+print(response)
