9183d0991328350ba57c5e03ae98bb1666389f20
Manabendu-ai <rikugithub@gmail.com>
2026-08-03 17:38:02 +0530

    Updated Document Processor With OCR Parser

--- backend/document_processing/processor.py
+++ backend/document_processing/processor.py
@@ -1,14 +1,17 @@
 from .router import DocumentRouter
 from .parser import PDFParser
-
+from .paddle_ocr import OCRParser
 
 class DocumentProcessor:
     def __init__(self):
         self.router = DocumentRouter()
         self.parser = PDFParser()
+        self.ocr = OCRParser()
 
     def extract_text(self, file_path):
         metrics = self.router.classify_document(file_path)
 
         if metrics.document_type.value == "digital":
             return self.parser.parse(file_path)
+        else:
+            return self.ocr.extract(file_path)
