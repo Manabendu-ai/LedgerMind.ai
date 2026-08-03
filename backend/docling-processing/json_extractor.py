8d63c71889eeb964c8202295140df5f57bc38fd3
Manabendu-ai <rikugithub@gmail.com>
2026-08-03 19:44:17 +0530

    JSON extractor

---
+++ backend/docling-processing/json_extractor.py
@@ -0,0 +1,14 @@
+from docling.document_converter import DocumentConverter
+
+
+class JsonExtractor:
+    def __init__(self):
+        self.converter = DocumentConverter()
+
+    def extract(self, file_path: str) -> dict:
+        try:
+            doc = self.converter.convert(file_path)
+            self.content = doc.document.export_to_dict()
+            return self.content
+        except Exception as e:
+            print(f"File Exception : {e}")
