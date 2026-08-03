91f6b499b12b52618baaebcb1fd0361b4607c458
Manabendu-ai <rikugithub@gmail.com>
2026-08-03 20:08:59 +0530

    save json file implementation

--- backend/docling_processing/json_extractor.py
+++ backend/docling_processing/json_extractor.py
@@ -1,5 +1,6 @@
 from docling.document_converter import DocumentConverter
 import os
+import json
 
 os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
@@ -9,8 +10,23 @@ class JsonExtractor:
 
     def extract(self, file_path: str) -> dict:
         try:
+            self.file_path = file_path
             doc = self.converter.convert(file_path)
             self.content = doc.document.export_to_dict()
             return self.content
         except Exception as e:
-            print(f"File Exception : {e}")
+            print(f"Exception : {e}")
+
+    def save(self, json: dict, persist_dir:str = "docs/"):
+        try:
+            os.makedirs(persist_dir, exist_ok=True)
+
+            file_name = self.file_path.split(".")[0].split("/")[1]
+            json_path = os.path.join(persist_dir, f"{file_name}.json")
+
+            with open(json_path, "w") as f:
+                json.dump(self.content, f, indent = 4)
+
+            print(f"[SUCCESS] File Saved at : {json_path}")
+        except Exception as e:
+                    print(f"Exception : {e}")
