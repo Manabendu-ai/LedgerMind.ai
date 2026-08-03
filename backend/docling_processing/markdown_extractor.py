0af5ae07e9ae17ac482e2cc13cfe51f39b02391b
Manabendu-ai <rikugithub@gmail.com>
2026-08-03 20:34:01 +0530

    Mardown Extractor

---
+++ backend/docling_processing/markdown_extractor.py
@@ -0,0 +1,18 @@
+from docling.document_converter import DocumentConverter
+import os
+
+os.environ["HF_TOKEN"] = os.getenv("HF_TOKEN")
+
+class MDExtractor:
+    def __init__(self):
+        self.converter = DocumentConverter()
+
+    def extract(self, file_path:str)->str:
+        try:
+            self.file_path = file_path
+            doc = self.converter.convert(file_path)
+            self.content = doc.document.export_to_markdown()
+            return self.content
+        except Exception as e:
+            print(f"Exception : {e}")
+        
