e992801d57f126392b7a86e046944d56a025ec33
Manabendu-ai <rikugithub@gmail.com>
2026-08-04 19:32:54 +0530

    final RAG Pipeline Testing -- OK report

--- rag_testing.py
+++ rag_testing.py
@@ -1,8 +1,13 @@
 from RAG.data_loader import DocumentLoader
 from RAG.embeddings import EmbeddingPipeline
+from RAG.search import RAGSearch
 
 if __name__ == "__main__":
-    docs = DocumentLoader().load_all_documents()
-    chunks  = EmbeddingPipeline().chunk_documents(docs)
-    vectors = EmbeddingPipeline().embed_chunks(chunks)
-    print(vectors)
+    # docs = DocumentLoader().load_all_documents()
+    # chunks  = EmbeddingPipeline().chunk_documents(docs)
+    # vectors = EmbeddingPipeline().embed_chunks(chunks)
+    # print(vectors)
+    rag_search = RAGSearch()
+    query = "What is the invoice total?"
+    summary = rag_search.search_and_summarize(query, top_k=3)
+    print("Summary:", summary)
