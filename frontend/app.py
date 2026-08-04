88a20d3fab10408311674c886d44486c8df7530b
Manabendu-ai <rikugithub@gmail.com>
2026-08-04 13:00:55 +0530

    created st.form for upload document

--- frontend/app.py
+++ frontend/app.py
@@ -13,6 +13,20 @@ st.markdown(
     #### Enterprise AI platform that transform invoices, receipts, tax documents, and financial records into structured intelligence.
     """
 )
-# st.divider()
-# with st.form(key = "upload_documnet"):
-#     st.markdown("##### Upload Your Document")
+st.divider()
+with st.form(key = "upload_documnet"):
+    left, center, right = st.columns([3,4,3])
+    with center:
+        st.markdown("##### Upload Your Document")
+
+        uploaded_file = st.file_uploader(
+            "Tax Invoices, Receipts, Bills",
+            type=["pdf"]
+        )
+        file_name = st.text_input("name of your excel file ")
+        submit_btn = st.form_submit_button(label="Genrate the excel sheet")
+
+if submit_btn:
+    reponse = ExcelGenerator(uploaded_file)
+    st.success(reponse)
+        
