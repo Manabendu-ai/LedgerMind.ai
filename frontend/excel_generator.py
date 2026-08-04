cc4b69ad013b887f60e407bad193dfc9c95c6091
Manabendu-ai <rikugithub@gmail.com>
2026-08-04 13:12:22 +0530

    implemented convert

--- frontend/excel_generator.py
+++ frontend/excel_generator.py
@@ -1,8 +1,8 @@
 from .config import EXCEL_CONVERTER_API
-
+import requests
 
 class ExcelGenerator:
-    def __init__(self, file):
+    def __init__(self, file, filename:str):
         self.files = {
                 "file" : (
                     file.name,
@@ -10,3 +10,20 @@ class ExcelGenerator:
                     file.type
                 )
             }
+        self.data = {
+            "excel_filename" : filename
+        }
+
+    def convert(self):
+        try:
+            response = requests.post(EXCEL_CONVERTER_API, files=self.files,data=self.data)
+            if response.status_code == 200:
+                return response.json()
+            else:
+                return {
+                        "status": "error",
+                        "status_code": response.status_code,
+                        "message": response.text
+                    }
+        except Exception as e:
+            print(f"[FILE UPLOAD ERROR] : {e}")
