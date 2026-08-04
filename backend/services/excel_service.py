aed2343d7613299c9ab75bd9d3b9d932d6f1e583
Manabendu-ai <rikugithub@gmail.com>
2026-08-04 11:12:36 +0530

    Added json_converter

--- backend/services/excel_service.py
+++ backend/services/excel_service.py
@@ -10,9 +10,16 @@ class ExcelService:
         self.model = ModelEngine()
         self.excel_gen = ExcelGenerator()
 
-    def markdown_generator(self):
-        self.extractor.extract(self.file_path)
-        self.md_path = self.extractor.save()
+    def markdown_generator(self)->str:
+        self.markdown = self.extractor.extract(self.file_path)
+        self.md_path = self.extractor.save("md_files/")
         return self.md_path
- 
+
+    def json_converter(self)->str:
+        self.response = self.model.run(self.markdown)
+        self.json_path = self.model.save(self.excel_filename, "json_files/")
+        return self.json_path
+
+    
+
     
