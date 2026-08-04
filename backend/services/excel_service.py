ff3e7e804ea287c285066b0d781ba061ebeb22ce
Manabendu-ai <rikugithub@gmail.com>
2026-08-04 10:53:17 +0530

    updated constructor of excel geenrator

--- backend/services/excel_service.py
+++ backend/services/excel_service.py
@@ -6,3 +6,8 @@ class ExcelService:
     def __init__(self, file_path: str, excel_filename:str):
         self.file_path = file_path
         self.excel_filename = excel_filename
+        self.extractor = MDExtractor()
+        self.model = ModelEngine()
+        self.excel_gen = ExcelGenerator()
+
+    
