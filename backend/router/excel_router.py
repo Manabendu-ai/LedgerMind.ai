e6ebb88eb8658e39590ea68f4ddc80e5cdd0d240
Manabendu-ai <rikugithub@gmail.com>
2026-08-04 10:33:15 +0530

    designed the layout of excel generator

--- backend/router/excel_router.py
+++ backend/router/excel_router.py
@@ -1,13 +1,19 @@
 from fastapi import APIRouter, UploadFile
 
-
 router = APIRouter(
     prefix="/excel",
     tags=["PDF 2 EXCEL"]
 )
 
 
-# @router.post("/generate")
-# async def excel_generator(file : UploadFile):
+@router.post("/generate")
+async def excel_generator(file : UploadFile, filename: str):
 
-#     file_path = await FileService().save(file)
+    file_path = await FileService().save(file)
+    excel_file_path = await ExcelService().convert(file_path, filename)
+
+    return {
+       "status" : "Excel File Generated Successfully",
+       "file" : filename, 
+       "saved_at" : excel_file_path
+    }
