d7bd79e37b3bcb1fca734aaa6c60cf9be94da708
Manabendu-ai <rikugithub@gmail.com>
2026-08-04 13:34:33 +0530

    updated session_state

--- frontend/app.py
+++ frontend/app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from .excel_generator import ExcelGenerator
+from excel_generator import ExcelGenerator
 st.set_page_config(
     page_title = "LedgerMind.ai",
     layout="centered",
@@ -23,10 +23,24 @@ with st.form(key = "upload_documnet"):
             "Tax Invoices, Receipts, Bills",
             type=["pdf"]
         )
-        file_name = st.text_input("name of your excel file ")
+        if uploaded_file is not None:
+            st.session_state.uploaded_file = uploaded_file
+        if "file_name" not in st.session_state:
+            st.session_state.file_name=""
+
+        st.session_state.file_name = st.text_input("name of your excel file ")
         submit_btn = st.form_submit_button(label="Genrate the excel sheet")
 
-if submit_btn:
-    reponse = ExcelGenerator(uploaded_file).convert()
-    st.success(reponse)
+        if submit_btn:
+            file = st.session_state.get("uploaded_file")
+            if file is None:
+                st.error("Please Upload a File!")
+            else:
+                reponse = ExcelGenerator(
+                    file, 
+                    st.session_state.get("file_name")
+                ).convert()
+                st.success(reponse)
+
+
         
